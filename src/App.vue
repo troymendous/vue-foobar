669104d46953a81b8de64be7ed631e427744ea1e
Troymendous <troy.wilson.was.taken@gmail.com>
2025-03-15 12:06:21 +1100

    Initial commit for Vue FooBar app

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>FooBar Vue App</h1>
+    <ul>
+      <li v-for="num in numbers" :key="num">{{ displayText(num) }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numbers: Array.from({ length: 100 }, (_, i) => i + 1),
+    };
+  },
+  methods: {
+    displayText(num) {
+      if (num % 3 === 0 && num % 5 === 0) return "FooBar";
+      if (num % 3 === 0) return "Foo";
+      if (num % 5 === 0) return "Bar";
+      return num;
+    },
+  },
+};
+</script>
+
+<style>
+h1 {
+  text-align: center;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  text-align: center;
+}
+</style>
